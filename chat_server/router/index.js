6d21aaa27b65a6bbdc8970052634d94d46f6ab28
snowbe11 <snowbe11@ncsoft.com>
2021-11-15 18:03:11 +0900

    서버 json 스키마 변경
    json() 이 되지 않는 오류를 체크하기 위해 변경했으나 최종 스펙으로 서밋함

--- chat_server/router/index.js
+++ chat_server/router/index.js
@@ -1,15 +1,15 @@
 const { Router } = require("express");
-const { rankManager } = require('../service');
+const { rankManager } = require("../service");
 
 const router = Router();
 
-router.get('/rank', (request, response) => {
-    console.log('client request rank');
+router.get("/rank", (request, response) => {
+  console.log("client request rank");
 
-    response.json({
-        result: 'success',
-        payload: rankManager.getRank()
-    });
+  response.json({
+    status: "success",
+    payload: rankManager.getRank(),
+  });
 });
 
 module.exports = router;
